93479eaa4faa90e78d35bdacf9bed9063bc41d14
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-04 17:13:25 +0300

    fix tzdata install

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+DEBIAN_FRONTEND=noninteractive
+
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
@@ -9,6 +11,8 @@ cp -rvf ./debian ./asusctl
 cd ./asusctl
 
 # Get build deps
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
